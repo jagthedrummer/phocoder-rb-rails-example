57fee7ff4dee83f1572dfa71cc59ee239de1cb8b
Jeremy Green <jagthedrummer@gmail.com>
2011-01-01 16:37:04 -0600

    Got basic s3 handling working.

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -1,7 +1,7 @@
 class ImageUpload < ActiveRecord::Base
-  
-  before_destroy :remove_local_file
-  after_save :save_file
+  require 'aws/s3'
+  before_destroy :remove_local_file,:remove_s3_file
+  after_save :save_local_file
   
   def file=(new_file)
     self.filename = new_file.original_filename
@@ -10,10 +10,10 @@ class ImageUpload < ActiveRecord::Base
   end
   
   
-  def save_file
+  def save_local_file
     return if @saved_file.blank?
     FileUtils.mkdir_p local_dir
-    FileUtils.cp @saved_file.tempfile.path, local_path
+    FileUtils.cp @saved_file.path, local_path
   end
   
   def remove_local_file
@@ -35,6 +35,40 @@ class ImageUpload < ActiveRecord::Base
     File.join(local_dir,filename)
   end
   
+  def local_url
+    File.join("/",resource_dir,filename)
+  end
+  
+  #S3 stuff
+  attr_reader :bucket_name
+  
+  def s3_key
+    File.join(resource_dir,filename)
+  end
+  
+  def s3_config
+    return @s3_config if !@s3_config.blank?
+    config_path =  RAILS_ROOT + '/config/amazon_s3.yml'
+    @s3_config =  YAML.load(ERB.new(File.read(config_path)).result)[RAILS_ENV].symbolize_keys
+    AWS::S3::Base.establish_connection!(
+      :access_key_id     => @s3_config[:access_key_id],
+      :secret_access_key => @s3_config[:secret_access_key]
+    )
+    @s3_config
+  end
+  
+  def save_s3_file
+    AWS::S3::S3Object.store(
+      s3_key, 
+      open(local_path), 
+      s3_config[:bucket_name],
+      :access => :public_read
+    )
+  end
+  
+  def remove_s3_file
+    AWS::S3::S3Object.delete s3_key, s3_config[:bucket_name]
+  end
   
   
 end
--- spec/models/image_upload_spec.rb
+++ spec/models/image_upload_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe ImageUpload do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = {
+      :file => Rack::Test::UploadedFile.new(fixture_path + '/big_eye_tiny.jpg', 'image/jpeg') 
+    }
+  end
+  
+  
+  it "should get the right file name" do
+    iu = ImageUpload.new(@attr)
+    iu.filename.should == "big_eye_tiny.jpg"
+  end
+  
+  it "should read AWS::S3 config" do
+    iu = ImageUpload.new(@attr)
+    iu.s3_config[:bucket_name].should == "test.phocoder-rb-rails-example.webapeel.com"
+  end
+  
+  
+  it "should save the file to a local storage location" do
+    iu = ImageUpload.new(@attr)
+    iu.save
+    expected_local_path = File.join(RAILS_ROOT,'public','ImageUpload',iu.id.to_s,iu.filename)
+    iu.local_path.should == expected_local_path
+    iu.local_url.should == "/ImageUpload/#{iu.id}/#{iu.filename}"
+    File.exists?(expected_local_path).should be_true
+    iu.destroy
+    File.exists?(expected_local_path).should_not be_true
+  end
+  
+  it "should save the file to an AWS S3 storage location" do
+    iu = ImageUpload.new(@attr)
+    iu.save
+    expected_local_path = File.join(RAILS_ROOT,'public','ImageUpload',iu.id.to_s,iu.filename)
+    iu.local_path.should == expected_local_path
+    iu.local_url.should == "/ImageUpload/#{iu.id}/#{iu.filename}"
+    File.exists?(expected_local_path).should be_true
+    #should not be on S3 yet
+    bucket = iu.s3_config[:bucket_name]
+    key = iu.s3_key
+    lambda{
+      o = AWS::S3::S3Object.find(key,bucket)
+    }.should raise_error(Exception)
+    #now store in S3
+    iu.save_s3_file
+    lambda{
+      o = AWS::S3::S3Object.find(key,bucket)
+    }.should_not raise_error(Exception)
+    
+    iu.destroy
+    lambda{
+      o = AWS::S3::S3Object.find(key,bucket)
+    }.should raise_error(Exception)
+    File.exists?(expected_local_path).should_not be_true
+  end
+  
+  
+  
+  
 end
